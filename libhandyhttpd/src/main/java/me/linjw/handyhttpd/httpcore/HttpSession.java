fad39bd7815f155e84a6999e61cbc9e7f60135f2
linjiawei <linjiawei3046@cvte.com>
2018-03-31 13:45:30 +0800

    [feature] supply HttpSessionTest

--- libhandyhttpd/src/main/java/me/linjw/handyhttpd/httpcore/HttpSession.java
+++ libhandyhttpd/src/main/java/me/linjw/handyhttpd/httpcore/HttpSession.java
@@ -4,6 +4,7 @@ import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
@@ -18,8 +19,10 @@ import me.linjw.handyhttpd.HandyHttpdServer;
  */
 
 
+@SuppressWarnings("WeakerAccess")
 public class HttpSession implements Runnable {
     public static final int BUF_SIZE = 8 * 1024;
+
     public static final int REQUEST_LINE_METHOD = 0;
     public static final int REQUEST_LINE_URI = 1;
     public static final int REQUEST_LINE_VERSION = 2;
@@ -36,6 +39,9 @@ public class HttpSession implements Runnable {
         mOutputStream = socket.getOutputStream();
     }
 
+    /**
+     * read http request and send http response in a subthread.
+     */
     @Override
     public void run() {
         mInputStream.mark(BUF_SIZE);
@@ -43,7 +49,7 @@ public class HttpSession implements Runnable {
 
         try {
             String[] requestLine = parseRequestLine(mInputStream, buff, BUF_SIZE);
-            Map<String, String> headers = parseHeaders(mInputStream, buff, BUF_SIZE);
+            Map<String, String> headers = parseHeaderFields(mInputStream, buff, BUF_SIZE);
 
 
             if (requestLine == null || requestLine.length < 3) {
@@ -66,9 +72,22 @@ public class HttpSession implements Runnable {
         }
     }
 
-    private String[] parseRequestLine(BufferedInputStream inputStream, byte[] buf, int bufSize)
+    /**
+     * parse request line from InputStream.
+     * Notice: this method will skip the InputStream when parse the request line.
+     *
+     * @param is      InputStream
+     * @param buf     a temp buffer
+     * @param bufSize buffer size
+     * @return request line: [METHOD,URI,VERSION] or null
+     * @throws IOException IOException
+     * @see HttpSession#REQUEST_LINE_METHOD
+     * @see HttpSession#REQUEST_LINE_URI
+     * @see HttpSession#REQUEST_LINE_VERSION
+     */
+    public static String[] parseRequestLine(InputStream is, byte[] buf, int bufSize)
             throws IOException {
-        int lineLength = getDataBySuffixs(inputStream, buf, bufSize, "\r\n");
+        int lineLength = moveDataWithSuffix(is, buf, bufSize, "\r\n");
         if (lineLength == 0) {
             return null;
         }
@@ -79,9 +98,19 @@ public class HttpSession implements Runnable {
         return reader.readLine().split(" ");
     }
 
-    private Map<String, String> parseHeaders(BufferedInputStream is, byte[] buf, int bufSize)
+    /**
+     * parse header fields from InputStream.
+     * Notice: this method will skip the InputStream when parse the header fields.
+     *
+     * @param is      InputStream
+     * @param buf     a temp buffer
+     * @param bufSize buffer size
+     * @return headers
+     * @throws IOException IOException
+     */
+    public static Map<String, String> parseHeaderFields(InputStream is, byte[] buf, int bufSize)
             throws IOException {
-        int headerEnd = getDataBySuffixs(is, buf, bufSize, "\r\n\r\n", "\n\n");
+        int headerEnd = moveDataWithSuffix(is, buf, bufSize, "\r\n\r\n", "\n\n");
         if (headerEnd == 0) {
             return null;
         }
@@ -106,50 +135,79 @@ public class HttpSession implements Runnable {
         return headers;
     }
 
-    private int getDataBySuffixs(BufferedInputStream is, byte[] buf, int bufSize, String... suffixs)
+    /**
+     * move data with one of the suffixs from InputStream to the buffer.
+     * Notice: this method will skip the InputStream when write data to buf.
+     *
+     * @param is      InputStream
+     * @param buf     buffer to write
+     * @param bufSize buffer size
+     * @param suffixs suffixs
+     * @return the size of data write to buffer
+     * @throws IOException IOException
+     */
+    @SuppressWarnings("ResultOfMethodCallIgnored")
+    public static int moveDataWithSuffix(InputStream is, byte[] buf, int bufSize, String... suffixs)
             throws IOException {
         int rlen = 0;
         int length = 0;
         int read = is.read(buf, 0, bufSize);
         while (read > 0) {
             rlen += read;
-            length = getLength(buf, rlen, suffixs);
+            length = findEnd(buf, rlen, suffixs);
             if (length > 0) {
                 break;
             }
             read = is.read(buf, rlen, bufSize - rlen);
         }
 
-        if (length < rlen) {
+        if (length > 0 && length < rlen) {
             is.reset();
             is.skip(length);
         }
 
-        return length;
+        return length > 0 ? length : 0;
     }
 
-    private int getLength(final byte[] buf, int size, String... suffixs) {
+    /**
+     * find the end pos of the first occur str with one of the suffixs in byte.
+     *
+     * @param data    data to search
+     * @param size    size of data array
+     * @param suffixs suffixs list
+     * @return the end pos. Notice it is the last index + 1
+     */
+    public static int findEnd(final byte[] data, int size, String... suffixs) {
         if (suffixs == null || suffixs.length <= 0) {
-            return 0;
+            return -1;
         }
 
         for (int i = 0; i < size; i++) {
             for (String suffix : suffixs) {
-                if (isEqual(buf, i, size, suffix)) {
+                if (isEqual(data, i, size, suffix)) {
                     return i + suffix.length();
                 }
             }
         }
-        return 0;
+        return -1;
     }
 
-    private boolean isEqual(final byte[] buf, int offset, int size, String str) {
+    /**
+     * compare data with string in buf level.
+     *
+     * @param data   byte buffer
+     * @param offset begin index of data to compare
+     * @param size   size of data array
+     * @param str    string to compare
+     * @return is equal or not
+     */
+    public static boolean isEqual(final byte[] data, int offset, int size, String str) {
         if (offset + str.length() - 1 >= size) {
             return false;
         }
 
         for (int i = 0; i < str.length(); i++) {
-            if (buf[offset + i] != str.charAt(i)) {
+            if (data[offset + i] != str.charAt(i)) {
                 return false;
             }
         }
---
+++ libhandyhttpd/src/test/java/me/linjw/handyhttpd/httpcore/HttpSessionTest.java
@@ -0,0 +1,181 @@
+package me.linjw.handyhttpd.httpcore;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
+import java.util.Map;
+
+import me.linjw.handyhttpd.HandyHttpdServer;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Matchers.any;
+import static org.powermock.api.mockito.PowerMockito.mock;
+
+/**
+ * Created by linjiawei on 2018/3/31.
+ * e-mail : bluesky466@qq.com
+ */
+@RunWith(PowerMockRunner.class)
+public class HttpSessionTest {
+    @Test
+    public void isEqual() {
+        String str = "hello world";
+        byte[] bytes = str.getBytes();
+
+        assertTrue(HttpSession.isEqual(bytes, 0, bytes.length, str));
+        assertTrue(HttpSession.isEqual(bytes, "hello ".getBytes().length, bytes.length, "world"));
+        assertFalse(HttpSession.isEqual(bytes, 1, bytes.length, "world"));
+    }
+
+    @Test
+    public void findEnd() {
+        byte[] bytes = "hello world\r\nhello world\r\n\r\n\n".getBytes();
+
+        assertEquals(
+                "hello world\r\n".getBytes().length,
+                HttpSession.findEnd(bytes, bytes.length, "\r\n")
+        );
+
+        assertEquals(
+                "hello world\r\nhello world\r\n\r\n".getBytes().length,
+                HttpSession.findEnd(bytes, bytes.length, "\r\n\r\n")
+        );
+
+        assertEquals(
+                bytes.length,
+                HttpSession.findEnd(bytes, bytes.length, "\n\n")
+        );
+
+        assertEquals(
+                -1,
+                HttpSession.findEnd(bytes, bytes.length, "\r\r")
+        );
+    }
+
+    @Test
+    public void getDataBySuffixs() throws IOException {
+        byte[] buf = new byte[1024];
+        String data = "hello world\r\nhello world\r\n\r\n";
+        InputStream is = new ByteArrayInputStream(data.getBytes());
+
+        assertEquals(
+                "hello world\r\n".getBytes().length,
+                HttpSession.moveDataWithSuffix(is, buf, buf.length, "\r\n")
+        );
+
+        //this method will skip the InputStream when write data to buf,
+        //so 'hello world\r\n' is skiped in the before step.
+        assertEquals(
+                "hello world\r\n\r\n".getBytes().length,
+                HttpSession.moveDataWithSuffix(is, buf, buf.length, "\r\n\r\n")
+        );
+
+        is.reset();
+        assertEquals(
+                data.getBytes().length,
+                HttpSession.moveDataWithSuffix(is, buf, buf.length, "\r\n\r\n")
+        );
+
+        is.reset();
+        assertEquals(
+                0,
+                HttpSession.moveDataWithSuffix(is, buf, buf.length, "\r\r")
+        );
+    }
+
+    @Test
+    public void parseHeaderFields() throws IOException {
+        String headerFields = "Host: tools.ietf.org\r\n" +
+                "Connection: keep-alive\r\n" +
+                "Pragma: no-cache\r\n" +
+                "Cache-Control: no-cache\r\n" +
+                "Upgrade-Insecure-Requests: 1\r\n" +
+                "Accept-Encoding: gzip, deflate, br\r\n" +
+                "Accept-Language: zh-CN,zh;q=0.9,en;q=0.8\r\n\r\n";
+
+        InputStream is = new ByteArrayInputStream(headerFields.getBytes());
+        byte[] buf = new byte[1024];
+        Map<String, String> header = HttpSession.parseHeaderFields(is, buf, buf.length);
+        assertNotNull(header);
+        assertEquals("tools.ietf.org", header.get("host"));
+        assertEquals("keep-alive", header.get("connection"));
+        assertEquals("no-cache", header.get("pragma"));
+        assertEquals("no-cache", header.get("cache-control"));
+        assertEquals("1", header.get("upgrade-insecure-requests"));
+        assertEquals("gzip, deflate, br", header.get("accept-encoding"));
+        assertEquals("zh-CN,zh;q=0.9,en;q=0.8", header.get("accept-language"));
+    }
+
+    @Test
+    public void parseRequestLine() throws IOException {
+        String headerFields = "GET /html/rfc2616 HTTP/1.1\r\n";
+
+        InputStream is = new ByteArrayInputStream(headerFields.getBytes());
+        byte[] buf = new byte[1024];
+
+        assertEquals(headerFields.getBytes().length, is.available());
+
+        String[] requestLine = HttpSession.parseRequestLine(is, buf, buf.length);
+        assertNotNull(requestLine);
+        assertEquals("GET", requestLine[HttpSession.REQUEST_LINE_METHOD]);
+        assertEquals("/html/rfc2616", requestLine[HttpSession.REQUEST_LINE_URI]);
+        assertEquals("HTTP/1.1", requestLine[HttpSession.REQUEST_LINE_VERSION]);
+
+        //this method will skip the InputStream when parse request line,
+        //so InputStream is empty now.
+        assertEquals(0, is.available());
+    }
+
+    @Test
+    @PrepareForTest({HandyHttpdServer.class, HttpResponse.class, HttpSession.class})
+    public void run() throws IOException {
+        String header = "GET /html/rfc2616?key=val HTTP/1.1\r\n" +
+                "Host: tools.ietf.org\r\n" +
+                "Connection: keep-alive\r\n" +
+                "Pragma: no-cache\r\n" +
+                "Cache-Control: no-cache\r\n" +
+                "Upgrade-Insecure-Requests: 1\r\n" +
+                "Accept-Encoding: gzip, deflate, br\r\n" +
+                "Accept-Language: zh-CN,zh;q=0.9,en;q=0.8\r\n\r\n";
+
+        Socket socket = mock(Socket.class);
+        given(socket.getInputStream()).willReturn(new ByteArrayInputStream(header.getBytes()));
+        given(socket.getOutputStream()).willReturn(new ByteArrayOutputStream(0));
+
+        HttpResponse response = mock(HttpResponse.class);
+        HandyHttpdServer server = mock(HandyHttpdServer.class);
+        given(server.onRequest(any(HttpRequest.class))).willReturn(response);
+
+        new HttpSession(server, socket).run();
+
+        ArgumentCaptor<HttpRequest> arg = ArgumentCaptor.forClass(HttpRequest.class);
+
+        then(server).should().onRequest(arg.capture());
+
+        HttpRequest request = arg.getValue();
+        assertEquals("GET", request.getMethod());
+        assertEquals("/html/rfc2616", request.getUri());
+        assertEquals("HTTP/1.1", request.getVersion());
+        assertEquals("tools.ietf.org", request.getHeaders().get("host"));
+        assertEquals("keep-alive", request.getHeaders().get("connection"));
+        assertEquals("no-cache", request.getHeaders().get("pragma"));
+        assertEquals("no-cache", request.getHeaders().get("cache-control"));
+        assertEquals("1", request.getHeaders().get("upgrade-insecure-requests"));
+        assertEquals("gzip, deflate, br", request.getHeaders().get("accept-encoding"));
+        assertEquals("zh-CN,zh;q=0.9,en;q=0.8", request.getHeaders().get("accept-language"));
+        assertEquals("val", request.getParams().get("key"));
+    }
+}
